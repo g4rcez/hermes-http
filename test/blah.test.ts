a8fa9d8e71a2ed2099f66037dc6e35ed8a1ee992
Garcez <allan.f.garcez@gmail.com>
2019-11-25 10:26:02 -0300

    update

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,5 +1,18 @@
+import fetch from "node-fetch";
+import Hermes, { ResponseFetch } from "../dist";
+
+const hermes = Hermes({
+	fetchInstance: fetch,
+	responseType: "application/json",
+	throwOnHttpError: false
+});
+
 describe("blah", () => {
 	it("works", () => {
-		expect(2).toEqual(2);
+		const url = "https://api.postmon.com.br/v1/cep/38706400";
+		hermes
+			.get(url)
+			.then((e) => e)
+			.catch((e: ResponseFetch) => e.data);
 	});
 });
