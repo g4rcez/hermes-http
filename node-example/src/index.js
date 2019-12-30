dffa108cee510448f578fafaf29aec847cb416e0
g4rcez <allan.f.garcez@gmail.com>
2019-12-30 01:04:25 -0300

    isomorphic fetch

--- node-example/src/index.js
+++ node-example/src/index.js
@@ -1,15 +1,13 @@
 //@ts-check
 const Hermes = require("hermes-http").default;
-const fetch = require("node-fetch");
 
-global.fetch = fetch;
-global.Headers = fetch.Headers;
-global.Response = fetch.Response;
-global.AbortController = require("abort-controller");
+const hermes = Hermes({ timeout: 30000 });
 
-const hermes = Hermes({
-	fetchInstance: fetch,
-	timeout: 30000
-});
-
-hermes.get("https://api.postmon.com.br/v1/cep/38706400").then(e => console.log(e)).catch(e => console.log(e))
+hermes
+	.get("https://api.postmon.com.br/v1/cep/38706400")
+	.then((e) => console.log(e))
+	.catch((e) => console.log(e));
+hermes
+	.get("https://api.github.com/users/octocat")
+	.then((e) => console.log(e))
+	.catch((e) => console.log(e));
