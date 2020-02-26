feafca046488ceea421610f3f8990d3702f6c77b
g4rcez <allan.f.garcez@gmail.com>
2020-02-25 22:54:45 -0300

    Handle http errors

--- src/plugins/http-errors.ts
+++ src/plugins/http-errors.ts
@@ -1,14 +1,58 @@
+import { ResponseError } from "../hermes-http-types";
+
 export const HttpErrorEnum = {
-	404: "NOT_FOUND"
+	100: "CONTINUE",
+	200: "OK",
+	201: "CREATED",
+	202: "ACCEPTED",
+	203: "NON_AUTHORITATIVE_INFORMATION",
+	204: "NO_CONTENT",
+	205: "RESET_CONTENT",
+	206: "PARTIAL_CONTENT",
+	301: "MOVE_PERMANENTLY",
+	302: "FOUND",
+	307: "TEMPORARY_REDIRECT",
+	308: "PERMANENT_REDIRECT",
+	400: "BAD_REQUEST",
+	401: "UNAUTHORIZED",
+	402: "PAYMENT_REQUIRED",
+	403: "FORBIDDEN",
+	404: "NOT_FOUND",
+	405: "METHOD_NOT_ALLOWED",
+	406: "NOT_ACCEPTABLE",
+	408: "REQUEST_TIMEOUT",
+	409: "CONFLICT",
+	429: "TOO_MANY_REQUESTS",
+	500: "INTERNAL_SERVER_ERROR",
+	501: "NOT_IMPLEMENTED",
+	502: "BAD_GATEWAY",
+	503: "SERVICE_UNAVAILABLE",
+	504: "GATEWAY_TIMEOUT"
 };
 
-type HttpCodes = keyof typeof HttpErrorEnum;
-export abstract class HttpError extends Error {
+export class HttpError {
 	public code: number;
+	public name: string;
+	public message: string | null;
 
-	public constructor(message: string, code: HttpCodes) {
-		super(message);
-		this.code = code;
-		this.name = code in HttpErrorEnum ? HttpErrorEnum[code] : "";
+	public constructor(response: ResponseError<unknown>) {
+		this.code = response.status;
+		this.name = this.code in HttpErrorEnum ? HttpErrorEnum[this.code] : "";
+		this.message = response.statusText;
 	}
 }
+
+const isHttpError = (response: ResponseError<unknown>) =>
+	typeof response.status === "number" && response.status >= 100 && response.status <= 599;
+
+export type HttpResponseError<T> = ResponseError<T> & {
+	httpError: HttpError | null;
+};
+
+export const httpErrorInterceptor = async <T>(response: ResponseError<T>) => {
+	const error = new HttpError(response);
+	if (isHttpError(response)) {
+		return { ...response, httpError: error };
+	}
+	return { ...response, httpError: null };
+};
