6dac9adbb4d8f04585e66e0bf82c6cbd69c46dab
g4rcez <allan.f.garcez@gmail.com>
2020-02-25 22:54:01 -0300

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { HermesHttp as default } from "./hermes";
+export * from "./plugins";
 export { ResponseError, HttpMethods, RedirectMode, CredentialsMode, CorsMode } from "./hermes-http-types";
