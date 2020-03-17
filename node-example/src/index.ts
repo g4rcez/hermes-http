646bfa392b73d085b1e71f2a61d09ebe89576e9e
g4rcez <allan.f.garcez@gmail.com>
2020-03-17 14:24:27 -0300

    Update example

--- node-example/src/index.ts
+++ node-example/src/index.ts
@@ -1,48 +1,16 @@
 import Hermes, { httpErrorInterceptor, HttpResponseError } from "hermes-http";
-type GithubUser = {
-	login: string;
-	id: number;
-	node_id: string;
-	avatar_url: string;
-	gravatar_id: string;
-	url: string;
-	html_url: string;
-	followers_url: string;
-	following_url: string;
-	gists_url: string;
-	starred_url: string;
-	subscriptions_url: string;
-	organizations_url: string;
-	repos_url: string;
-	events_url: string;
-	received_events_url: string;
-	type: string;
-	site_admin: false;
-	name: string;
-	company: string;
-	blog: string;
-	location: string;
-	email: null;
-	hireable: null;
-	bio: string | null;
-	public_repos: number;
-	public_gists: number;
-	followers: number;
-	following: number;
-	created_at: string;
-	updated_at: string;
-};
 
 const hermes = Hermes({ avoidDuplicateRequests: true }).errorResponseInterceptor(httpErrorInterceptor);
 
-const request = (n: number) =>
-	hermes
-		.get<GithubUser>("https://api.github.com/users/octocatx" + n, { timeout: 1000 })
-		.then(console.info)
-		.catch((e: HttpResponseError<unknown>) => {
-			if (e.httpError !== null) {
-				console.log({ ...e });
-			}
-		});
-
-request(2).catch(console.error);
+hermes
+	.get<{ results: string[] }>("https://swapi.co/api/people", {
+		query: {
+			search: "r2"
+		}
+	})
+	.then((e) => console.info(e))
+	.catch((e: HttpResponseError<unknown>) => {
+		if (e.httpError !== null) {
+			console.log({ ...e });
+		}
+	});
