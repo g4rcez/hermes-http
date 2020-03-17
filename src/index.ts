cf0921d99414ad13431d18d8be93b2778e797ef4
g4rcez <allan.f.garcez@gmail.com>
2020-03-17 14:30:09 -0300

    exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { HermesHttp as default } from "./hermes";
 export * from "./plugins";
+export * from "./helpers/http-codes";
 export { ResponseError, HttpMethods, RedirectMode, CredentialsMode, CorsMode } from "./hermes-http-types";
